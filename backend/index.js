f2d649c72e6437e8cc519d2cfb0f09dac02cfaf4
Ayushman Singh Rathore <78416328+ayushman-s-rathore@users.noreply.github.com>
2024-06-28 15:45:00 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 const corsOption={
-    origin: 'http://localhost:5173',
+    origin: 'https://netflix-clone-kappa-beryl.vercel.app/',
     credentials: true
 }
 app.use(cors(corsOption))
